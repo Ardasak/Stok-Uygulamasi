0f906471d737eca2e657584e6bf83f414b113a57
Arda Sak <ardasak434@gmail.com>
2021-12-02 10:05:45 +0300

    Initial commit.

---
+++ StokUygulamasi/Form1.Designer.cs
@@ -0,0 +1,308 @@
+﻿
+namespace StokUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.mazToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aBCAnaliziToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.çıkışToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ekle = new System.Windows.Forms.Button();
+            this.sil = new System.Windows.Forms.Button();
+            this.guncelle = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mazToolStripMenuItem,
+            this.aBCAnaliziToolStripMenuItem,
+            this.çıkışToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1299, 36);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // mazToolStripMenuItem
+            // 
+            this.mazToolStripMenuItem.Name = "mazToolStripMenuItem";
+            this.mazToolStripMenuItem.Size = new System.Drawing.Size(142, 29);
+            this.mazToolStripMenuItem.Text = "Malzeme Kayıt";
+            // 
+            // aBCAnaliziToolStripMenuItem
+            // 
+            this.aBCAnaliziToolStripMenuItem.Name = "aBCAnaliziToolStripMenuItem";
+            this.aBCAnaliziToolStripMenuItem.Size = new System.Drawing.Size(117, 29);
+            this.aBCAnaliziToolStripMenuItem.Text = "ABC Analizi";
+            // 
+            // çıkışToolStripMenuItem
+            // 
+            this.çıkışToolStripMenuItem.Name = "çıkışToolStripMenuItem";
+            this.çıkışToolStripMenuItem.Size = new System.Drawing.Size(64, 29);
+            this.çıkışToolStripMenuItem.Text = "Çıkış";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(69, 72);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1045, 220);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Malzeme Bilgileri";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(804, 148);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(111, 26);
+            this.textBox6.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(644, 151);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(155, 20);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Tedarik Süresi (gün):";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(804, 99);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(111, 26);
+            this.textBox5.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(642, 102);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(159, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Minimum Stok (adet):";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(804, 54);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(111, 26);
+            this.textBox4.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(642, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(154, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Birim Fiyat (TL/adet):";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(334, 142);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(111, 26);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(200, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(130, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Yıllık Satış (adet):";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(334, 96);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(111, 26);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(200, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Malzeme Adı:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(334, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(111, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(200, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Malzeme Kodu:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeight = 34;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Location = new System.Drawing.Point(69, 343);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(996, 292);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // ekle
+            // 
+            this.ekle.Location = new System.Drawing.Point(1136, 72);
+            this.ekle.Name = "ekle";
+            this.ekle.Size = new System.Drawing.Size(133, 49);
+            this.ekle.TabIndex = 3;
+            this.ekle.Text = "Ekle";
+            this.ekle.UseVisualStyleBackColor = true;
+            this.ekle.Click += new System.EventHandler(this.ekle_Click);
+            // 
+            // sil
+            // 
+            this.sil.Location = new System.Drawing.Point(1136, 157);
+            this.sil.Name = "sil";
+            this.sil.Size = new System.Drawing.Size(133, 49);
+            this.sil.TabIndex = 4;
+            this.sil.Text = "Sil";
+            this.sil.UseVisualStyleBackColor = true;
+            this.sil.Click += new System.EventHandler(this.sil_Click);
+            // 
+            // guncelle
+            // 
+            this.guncelle.Location = new System.Drawing.Point(1136, 243);
+            this.guncelle.Name = "guncelle";
+            this.guncelle.Size = new System.Drawing.Size(133, 49);
+            this.guncelle.TabIndex = 5;
+            this.guncelle.Text = "Güncelle";
+            this.guncelle.UseVisualStyleBackColor = true;
+            this.guncelle.Click += new System.EventHandler(this.guncelle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1299, 660);
+            this.Controls.Add(this.guncelle);
+            this.Controls.Add(this.sil);
+            this.Controls.Add(this.ekle);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Malzeme Kayıt";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem mazToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aBCAnaliziToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem çıkışToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button ekle;
+        private System.Windows.Forms.Button sil;
+        private System.Windows.Forms.Button guncelle;
+    }
+}
+
---
+++ StokUygulamasi/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StokUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-95T7H1G; Initial Catalog=Stok; Integrated Security=True");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ekle_Click(object sender, EventArgs e)
+        {
+            try {
+                String t1 = textBox1.Text;
+                String t2 = textBox2.Text;
+                String t3 = textBox3.Text;
+                String t4 = textBox4.Text;
+                String t5 = textBox5.Text;
+                String t6 = textBox6.Text;
+                if (!(String.IsNullOrEmpty(t1) || String.IsNullOrEmpty(t2) || String.IsNullOrEmpty(t3) || String.IsNullOrEmpty(t4) || String.IsNullOrEmpty(t5) || String.IsNullOrEmpty(t6))) {
+                    baglanti.Open();
+                    SqlCommand komut = new SqlCommand("INSERT INTO Malzemeler (MalzemeKodu, MalzemeAdi, YillikSatis, BirimFiyat, MinStok, TSuresi) VALUES (@data1,@data2,@data3,@data4,@data5,@data6)", baglanti);
+                    komut.Prepare();
+                    komut.Parameters.Add("@data1", SqlDbType.VarChar).Value = t1;
+                    komut.Parameters.Add("@data2", SqlDbType.VarChar).Value = t2;
+                    komut.Parameters.Add("@data3", SqlDbType.VarChar).Value = t3;
+                    komut.Parameters.Add("@data4", SqlDbType.VarChar).Value = t4;
+                    komut.Parameters.Add("@data5", SqlDbType.VarChar).Value = t5;
+                    komut.Parameters.Add("@data6", SqlDbType.VarChar).Value = t6;
+                    komut.ExecuteNonQuery();
+                    baglanti.Close();
+                    listele();
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Veri tabanına veri eklerken bir hata oluştu.", "Hata!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            }
+
+        private void sil_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                String malzeme_kod = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                baglanti.Open();
+                SqlCommand sqlCommand = new SqlCommand("DELETE FROM Malzemeler WHERE MalzemeKodu=@data1", baglanti);
+                sqlCommand.Prepare();
+                sqlCommand.Parameters.Add("@data1", SqlDbType.VarChar).Value = malzeme_kod;
+                sqlCommand.ExecuteNonQuery();
+                baglanti.Close();
+                listele();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Veri tabanından silerken bir hata oluştu.","Hata!",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+
+        private void guncelle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                String eski_malzeme_kod = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+                String t1 = textBox1.Text;
+                String t2 = textBox2.Text;
+                String t3 = textBox3.Text;
+                String t4 = textBox4.Text;
+                String t5 = textBox5.Text;
+                String t6 = textBox6.Text;
+                if (!(String.IsNullOrEmpty(t1) || String.IsNullOrEmpty(t2) || String.IsNullOrEmpty(t3) || String.IsNullOrEmpty(t4) || String.IsNullOrEmpty(t5) || String.IsNullOrEmpty(t6)))
+                    {
+                        baglanti.Open();
+                        SqlCommand sqlCommand = new SqlCommand("UPDATE Malzemeler SET MalzemeKodu=@data1 , MalzemeAdi=@data2 , YillikSatis=@data3 , BirimFiyat=@data4 , MinStok=@data5 , TSuresi=@data6 WHERE MalzemeKodu=@data7", baglanti);
+                        sqlCommand.Prepare();
+                        sqlCommand.Parameters.Add("@data1", SqlDbType.VarChar).Value = t1;
+                        sqlCommand.Parameters.Add("@data2", SqlDbType.VarChar).Value = t2;
+                        sqlCommand.Parameters.Add("@data3", SqlDbType.VarChar).Value = t3;
+                        sqlCommand.Parameters.Add("@data4", SqlDbType.VarChar).Value = t4;
+                        sqlCommand.Parameters.Add("@data5", SqlDbType.VarChar).Value = t5;
+                        sqlCommand.Parameters.Add("@data6", SqlDbType.VarChar).Value = t6;
+                        sqlCommand.Parameters.Add("@data7", SqlDbType.VarChar).Value = eski_malzeme_kod;
+                        sqlCommand.ExecuteNonQuery();
+                        baglanti.Close();
+                        listele();
+                    }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Veri tabanını güncellerken bir hata oluştu.", "Hata!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listele();
+        }
+
+        private void listele()
+        {
+            try
+            {
+                baglanti.Open();
+                SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("SELECT * FROM Malzemeler", baglanti);
+                DataTable table = new DataTable();
+                sqlDataAdapter.Fill(table);
+                dataGridView1.DataSource = table;
+                baglanti.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Veri tabanından veri çekerken bir hata oluştu.", "Hata!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            textBox1.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.CurrentRow.Cells[2].Value.ToString();
+            textBox4.Text = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+            textBox5.Text = dataGridView1.CurrentRow.Cells[4].Value.ToString();
+            textBox6.Text = dataGridView1.CurrentRow.Cells[5].Value.ToString();
+        }
+    }
+}
